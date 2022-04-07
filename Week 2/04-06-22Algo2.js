059979e495664cde53c853568eeb02b63775a0e9
Muhammad Shahbaz <M023Shah@gmail.com>
2022-04-06 17:59:30 -0700

    algo

---
+++ Week 2/04-06-22Algo2.js
@@ -0,0 +1,16 @@
+//first Recursion without built in function
+
+function indexAtFirstRepetetion(str) {
+    //An integer to store presence/absence
+    //of 26 characters using its 32 bits
+    let checkerVariable = 0;
+
+    //for loop to iterate through string and check for repeating character
+
+    for ( let i = 0; i < String.length; i++) {
+        let ForLoopValueOfString = (str[i])
+
+        if (ForLoopValueOfString = ) 
+    }
+
+}
