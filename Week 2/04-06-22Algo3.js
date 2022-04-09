160228a70b6da54d059c8ea024859ee93d46926a
Muhammad Shahbaz <M023Shah@gmail.com>
2022-04-09 12:58:29 -0700

    algo

---
+++ Week 2/04-06-22Algo3.js
@@ -0,0 +1,15 @@
+// Insertion Sort Algo
+
+function insertionSort(arr){
+
+    for (var i = 1; i < arr.length; i++){
+        var currentVal = arr[i];
+        for(var j = i -1; j > -1, j--){
+            arr[j] currentVal
+        }
+    }
+
+    return arr;
+}
+
+insertionSort([2,1,9,76,4])
