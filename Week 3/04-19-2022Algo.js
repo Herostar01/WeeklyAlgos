5972e883d4018707f766682962dba58033a40de8
Muhammad Shahbaz <M023Shah@gmail.com>
2022-04-19 09:21:55 -0700

    quickSort

--- Week 3/04-19-2022Algo.js
+++ Week 3/04-19-2022Algo.js
@@ -16,3 +16,21 @@ function BubbleSortFromMemory(arr){
 }
 
 console.log(BubbleSortFromMemory([8,23,98,34,12,55,34]))
+
+//More efficient version
+function BubbleSortQuick(arr){
+    for(var a = arr.length; a > 0; a--){
+        for(var b = 0; b < a-1; b++){
+            console.log(arr, arr[b], arr[b+1])
+            if(arr[b] > arr[b+1]){
+                //swap!
+                var temp = arr[b]
+                arr[b] = arr[b+1]
+                arr[b+1] = temp
+            }
+        }
+    }
+    return arr;
+}
+
+console.log(BubbleSortQuick([4,9,2,88,3,23,90,34]))
