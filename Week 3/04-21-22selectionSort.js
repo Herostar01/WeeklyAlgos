aba97de785e5cda075741e4abee900a0da8535b1
Muhammad Shahbaz <M023Shah@gmail.com>
2022-04-21 16:21:33 -0700

    selectionSortAlgo

---
+++ Week 3/04-21-22selectionSort.js
@@ -0,0 +1,54 @@
+// Selection Sort
+
+function selectionSort(arr){
+    for(var a = 0; a < arr.length; a++){
+        var lowest = a;
+        for(var b = a+1; b < arr.length; b++){
+            if(arr[b] < arr[lowest]){
+                lowest = b;
+            }
+        }
+        console.log(arr, arr[b], arr[lowest])
+        console.log("SWAPPING TO: ")
+        var temp = arr[a];
+        arr[a] = arr[lowest];
+        arr[lowest] = temp;
+        console.log(arr)
+    }
+    
+    return arr;
+}
+
+selectionSort([0,2,34,22,10,19,17]);
+// i,j, lowest
+// 0,1, 1
+// 0,2, 2
+// 0,3, 2
+// 0,4, 2
+
+
+// Selection Sort
+
+function selectionSort(arr){
+    for(var a = 0; a < arr.length; a++){
+        var lowest = a;
+        for(var b = a+1; b < arr.length; b++){
+            if(arr[b] < arr[lowest]){
+                lowest = b;
+            }
+        }
+        if(a !== lowest) {
+            
+            console.log(arr, arr[b], arr[lowest])
+            console.log("SWAPPING TO: ")
+            var temp = arr[a];
+            arr[a] = arr[lowest];
+            arr[lowest] = temp;
+            
+        }
+    }
+    
+    return arr;
+}
+
+selectionSort([0,2,34,22,10,19,17]);
