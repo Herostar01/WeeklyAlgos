37c06baf91eb8a9fe5692e1e0a43f5470af14a49
Muhammad Shahbaz <M023Shah@gmail.com>
2022-04-25 09:24:46 -0700

    more Algo Prac

--- Week 3/04-25-22BubSortAFM.js
+++ Week 3/04-25-22BubSortAFM.js
@@ -2,15 +2,15 @@
 
 function BubbleSortAFM(arr){
 
-    const SWAP = (arr, idx1, idx2) => {
+    const swap = (arr, idx1, idx2) => {
         [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]]
     };
 
-    for(var a = 0; a < arr.length; a++){
-        for(b= 0; b < arr.length; b++){
+    for(var a = arr.length; a > 0; a--){
+        for(b= 0; b < a-1; b++){
             if(arr[b] > arr[b+1]){
-                //SWAP function
-                SWAP(arr, arr[b], arr[b+1])
+                //swap function
+                swap(arr, b, b+1)
             }
         }
     }
@@ -48,3 +48,99 @@ function BubbleSortAFMV2(arr){
 }
 
 BubbleSortAFMV2([-5,76, 2, 9, 33, 28, 12]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function BubbleSortAFMV3(arr){
+
+    for(x = arr.length; x > 0; x--){
+        for(y = 0; y < x-1; y++){
+            if (arr[y] > arr[y+1]){
+                //SWAP
+                var temp = arr[y];
+                arr[y] = arr[y+1];
+                arr[y+1] = temp;
+            }
+        }
+        console.log(arr)
+        console.log("********")
+    }
+
+    console.log(arr)
+    return arr;
+}
+
+BubbleSortAFMV3([3, 19, 34, 12, -4, -99, 49, -69]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function BubbleSortAFMV4(arr){
+
+    const SWAP = (arr, idx1, idx2) => {
+        [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+    }
+
+    for(x = 0; x < arr.length; x++){
+        for(y = 0; y < arr.length; y++){
+            if(arr[y] > arr[y+1]) {
+                //SWAP
+                SWAP(arr, y, y+1);
+            }
+        }
+        console.log(arr)
+        console.log("********")
+    }
+
+    console.log(arr);
+    return arr;
+}
+
+BubbleSortAFMV4([3, 19, 34, 12, -4, -99, 49, -69]);
