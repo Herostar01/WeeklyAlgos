258a64965d5d39f7c5e37c5d95c6e9ead4ff046f
Muhammad Shahbaz <M023Shah@gmail.com>
2022-05-16 18:41:33 -0700

    more practice

--- Week 5/practiceAlgo.js
+++ Week 5/practiceAlgo.js
@@ -16,7 +16,7 @@ function SS(arr){
             var temp = arr[a];
             arr[a] = arr[lowest];
             arr[lowest] = temp;
-            
+
         }
 
     }
@@ -26,3 +26,45 @@ function SS(arr){
 }
 
 SS([5,7,1,99,34,-33])
+
+//Bubble Sort Algo
+
+function BS(arr){
+
+    for( var a = 0; a < arr.length; a++) {
+        for ( var b = 0; b < arr.length; b++){
+            
+            if ( arr[b] > arr[b+1] ) {
+                //SWAP!
+                var temp = arr[b];
+                arr[b] = arr[b+1];
+                arr[b+1] = temp;
+            }
+        }
+    }
+
+    console.log(arr);
+    return arr;
+}
+BS([4,6,2,90,-33,-44,26])
+
+//Bubble Sort smart way
+
+function BSort(arr){
+
+    for ( var a = arr.length; a > 0; a--){
+        for ( var b = 0; b < a - 1; b++){
+
+            if ( arr[b] > arr[b+1]) {
+                //SWAP!
+                var temp = arr[b];
+                arr[b] = arr[b+1];
+                arr[b+1] = temp;
+            }
+        }
+    }
+
+    console.log(arr);
+    return arr;
+}
+BSort([4,6,2,90,-33,-44,26])
