6f2f125b14ce28f34ce985d296603fe17de81253
Muhammad Shahbaz <M023Shah@gmail.com>
2022-07-21 12:59:13 -0700

    reverseString

--- Week 6/algoCourse.js
+++ Week 6/algoCourse.js
@@ -9,4 +9,30 @@ function convertCtoF(num){
     return answer;
 }
 
-convertCtoF(-30)
+convertCtoF(-30);
+
+convertCtoF(-10);
+
+convertCtoF(20);
+
+convertCtoF(30);
+
+//Reverse a String
+
+function ReverseString(string){
+
+    var ReverseStr = "";
+    var arr = [];
+
+    for (T = string.length - 1; T >= 0; T--){
+        console.log(T);
+        console.log(string[T]);
+        arr.push(string[T])
+    }
+
+    console.log(arr);
+    return arr;
+}
+
+ReverseString("HELLO")
+
